751f2f56308b141fd5a0550317c5a8f8dd09f663
John Adedoyin A <oluwasomijiadewumi2@gmail.com>
2020-10-01 16:01:21 -0700

    new-update-2module4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,8 +15,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => {
-      return <Album>response.json();
-    });
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
